10ffa89e27e754e99eace7e17391f245b3bc8b87
Vikash Kumar <vikash.iitb@gmail.com>
2020-04-18 02:33:46 +0530

    Fixing the push

--- github-docker-pkg/index.js
+++ github-docker-pkg/index.js
@@ -18,19 +18,20 @@ var process = async function() {
         await exec.exec(`docker login docker.pkg.github.com -u ${username} -p ${token}`);
     } catch (err) {
         core.setFailed(`Could not login to github packages: ${err}`);
+        return
     }
 
 
     // Build the docker container with tag
+    const tag = github.context.ref.slice(10)
+    const repo = github.context.repo.repo.toLowerCase()
+    const dockerURL = `docker.pkg.github.com/${repo}:${tag}`
+    core.info("Git tag Found: " + tag)
     try {
-        const tag = github.context.ref.slice(10)
-        const repo = github.context.repo.repo.toLowerCase()
-        const dockerURL = `docker.pkg.github.com/${repo}:${tag}`
-        core.info("Git tag Found: " + tag)
-
         await exec.exec(`docker build -t ${dockerURL} .`);
     } catch (err) {
         core.setFailed(`Could not build the docker container. ${err}`)
+        return
     }
 
     // Push the container
@@ -38,6 +39,7 @@ var process = async function() {
         await exec.exec(`docker push ${dockerURL}`);
     } catch (err) {
         core.setFailed(`Docker Push Failed. ${err}`);
+        return
     }
 
     core.setOutput("docker-url", dockerURL)
